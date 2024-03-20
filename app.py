0109d238566d9a4d68a533b6dea898de03579e1a
Matheus Moreno Sá <mms.matheus1@gmail.com>
2024-03-20 18:38:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import re
 from datetime import datetime
 import matplotlib.pyplot as plt
 from scipy.stats import binom
-import s3_management
+#import s3_management
 from io import StringIO
 import dynamodb as db
 import streamlit_authenticator as stauth
